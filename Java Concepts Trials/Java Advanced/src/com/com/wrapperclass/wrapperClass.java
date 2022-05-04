5748682e1a8d02ee9a3e7a283b7b99cc7eb2d7dc
MeenuSowjanya <buvaneswarichandrasekaran.com>
2022-05-04 15:27:45 +0530

    Added wrapper class code

---
+++ Java Concepts Trials/Java Advanced/src/com/com/wrapperclass/wrapperClass.java
@@ -0,0 +1,21 @@
+package com.com.wrapperclass;
+class Boxing{
+    public static void boxing() {
+        int i=10;
+        Integer iObj1=Integer.valueOf(i);     // Boxing
+        Integer iObj2=i;                     // Autoboxing
+        System.out.println("Boxing : "+iObj1);
+        System.out.println("Autoboxing : "+iObj2);
+    }
+}
+
+class Unboxing{
+    public static void unBoxing() {
+        Integer iObj3=new Integer(10);
+        int j=iObj3.intValue();            // Unboxing
+        int p=iObj3;                      // Autounboxing
+        System.out.println("Unboxing : "+j);
+        System.out.println("Autoonboxing : "+p);
+    }
+}
+
